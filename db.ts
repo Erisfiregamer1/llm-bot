18e253c1ddda9a8d9e162fd6e6c8f5041361ff6c
Eris Starmaster <eris@erisws.com>
2023-06-18 11:58:12 -0400

    das

--- db.ts
+++ db.ts
@@ -3,7 +3,9 @@ import { default as Keyv } from "npm:keyv";
 import { KeyvFile } from "npm:keyv-file"
 
 const keyv = new Keyv({
-    store: new KeyvFile()
+    store: new KeyvFile({
+        filename: "./db.json"
+    })
 });
 
 export { keyv };
