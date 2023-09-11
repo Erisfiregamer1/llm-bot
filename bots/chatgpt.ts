243567b1ebc9849d855ab9129f29d5b502c6d55f
Eris Starmaster <eris@erisws.com>
2023-09-10 20:47:38 -0400

    WARNING: code has intentional bug for Deno repro

--- bots/chatgpt.ts
+++ bots/chatgpt.ts
@@ -1,5 +1,8 @@
 import { CreateChatCompletionResponse, ChatCompletionResponseMessage } from "npm:openai"
 
+import { config } from "npm:dotenv";
+config();
+
 export let isEnabled = true
 
 if (!Deno.env.get("OPENAI_API_KEY")) {
@@ -7,6 +10,44 @@ if (!Deno.env.get("OPENAI_API_KEY")) {
   isEnabled = false
 }
 
-async function send(messages: ChatCompletionResponseMessage[], prompt: string, userid: string): Promise<CreateChatCompletionResponse> {
-  
+type response = {
+  oaires: CreateChatCompletionResponse,
+  messages: ChatCompletionResponseMessage[]
+}
+
+// const db = await Deno.openKv("./db.sqlite")
+
+export async function send(messages: ChatCompletionResponseMessage[], prompt: string, userid: string): Promise<response> {
+  // here we go
+
+  if (messages.length === 0) {
+    messages.push({
+      role: "system",
+      content: "You are ChatGPT, an LLM by OpenAI."
+    })
+  }
+
+  messages.push({
+    role: "user",
+    content: prompt
+  })
+
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${Deno.env.get("OPENAI_API_KEY")}`
+    }, 
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo-16k",
+      messages: messages,
+      user: userid
+    })
+  })
+
+  const resp: CreateChatCompletionResponse = await res.json()
+
+  return {
+    oaires: resp,
+    messages
+  }
 }
--- main.ts
+++ main.ts
@@ -1,23 +1,93 @@
 import * as chatgpt from "./bots/chatgpt.ts";
-import * as bing_chat from "./bots/bing_chat.ts";
-import * as gpt4 from "./bots/gpt_4.ts";
-import * as palm from "./bots/palm.ts";
+// import * as bing_chat from "./bots/bing_chat.ts";
+// import * as gpt4 from "./bots/gpt_4.ts";
+// import * as palm from "./bots/palm.ts";
+
+import { ChatCompletionResponseMessage } from "npm:openai"
+
+type messagedata = {
+  id: string, 
+  messages: ChatCompletionResponseMessage[]
+}
 
 import client from "./client.ts"
 
-import "./slashcode.ts"
+// import "./slashcode.ts"
 
 import { ChannelType } from "npm:discord.js";
 
-const db = Deno.openKv("./db.sqlite")
+const db = await Deno.openKv("./db.sqlite")
+
+/*
+New database example:
+
+{
+  users: {
+    0: {
+      current_bot: "chatgpt",
+      current_conversation: 0,
+      conversations: {
+        chatgpt: [
+          {
+            id: "completion-37",
+            messages: [{}] // Alan, insert message object every time you finish here. Wait, Alan, are you still on the team?
+          }
+        ]
+      }
+    }
+  },
+}
+*/
 
 client.on("messageCreate", async (message) => {
     if (message.author.bot || JSON.stringify(message.flags) === "4096") return; // The "4096" flag is the @silent flag on discord.
-    if (message.channel.type === ChannelType.DM || false) { // DBTODO: Reimplement
-      chatgpt.send()
+    if (message.channel.type === ChannelType.DM || false) {
+
+      let error = false // Tracks if we've already pestered the user with an error / message :\
+
+      let llm = (await db.get<string>(["users", message.author.id, "current_bot"])).value // After reading the typedocs I realized this is the cleaner way to do this
+
+      if (llm === null) {
+        // They haven't used the bot before
+        llm = "chatgpt"
+        await db.set(["users", message.author.id, "current_bot"], llm)
+        await message.reply("Looks like this is your first time using this bot! Run /info to learn how to use the full potential of this bot.")
+        error = true
+      } else if (!llm.match(/^(chatgpt|bing|bard|gpt4)$/g)) {
+        // current LLM is corrupt. notify user and reset
+        llm = "chatgpt"
+        await db.set(["users", message.author.id, "current_bot"], llm)
+        await message.reply("Your current LLM is corrupted or removed! We've reset you to ChatGPT for now.")
+        error = true
+      }
+
+      let curconv = (await db.get<number>(["users", message.author.id, "current_conversation"])).value 
+
+      if (curconv === null) {
+        // They haven't used this LLM before 
+        curconv = 0
+        await db.set(["users", message.author.id, "current_conversation"], curconv)
+      }
+
+      let messages = (await db.get<messagedata[]>(["users", message.author.id, "conversations", llm])).value
+
+      if (messages === null) {
+        // No conversations for this LLM.
+        messages = [{
+          id: "New Conversation",
+          messages: []
+        }]
+          await db.set(["users", message.author.id, "conversations", llm], messages)
+      }
+
+      const curmsgs = messages[curconv].messages
+
+      const msg = await message.reply("Sending message...")
+
+      const resp = await chatgpt.send(curmsgs, message.content, message.author.id)
+
+      msg.edit(resp.oaires.choices[0].message?.content!)
     }
   });
   
   console.log("The bot is now online!");
-  
-
