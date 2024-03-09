2ac7dacd638dc6339b9ff285aa59fa31b871be45
eris <eris@erisws.com>
2024-03-09 00:01:30 +0000

    IDX

---
+++ .idx/dev.nix
@@ -0,0 +1,12 @@
+
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.deno
+  ];
+
+}
